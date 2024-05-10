ef1c46e136dc90cc28ac71742d8d0debe012f7f2
luischehin <luischehinok@gmail.com>
2024-05-09 22:24:22 -0300

    se creo la carpeta funciones

---
+++ teorica/funciones/funciones.js
@@ -0,0 +1,15 @@
+// EJERCICIO DE FUNCIONES 
+// FUNCION PARA SUMAR DOS NUMEROS 
+
+function sumar(a, b) {
+    return (a + b)
+}
+console.log(sumar(5 + 8));
+
+
+// FUNCION PARA RESTAR 
+
+function restar(a, b) {
+    return a - b;
+}
+console.log(restar(10 - 4))
